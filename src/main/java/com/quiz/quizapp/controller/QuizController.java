3a1f46408d350dc64277cc012b7ac3b855838a0b
naveen <naveendav2000@gmail.com>
2024-08-25 22:54:46 +0530

    Quiz app - without microservices

--- src/main/java/com/quiz/quizapp/controller/QuizController.java
+++ src/main/java/com/quiz/quizapp/controller/QuizController.java
@@ -1,12 +1,13 @@
 package com.quiz.quizapp.controller;
 
+import com.quiz.quizapp.model.QuestionWrapper;
+import com.quiz.quizapp.model.Response;
 import com.quiz.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -15,8 +16,18 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
+    @GetMapping("{id}")
+    public ResponseEntity<List<QuestionWrapper>> getList(@PathVariable int id) {
+        return quizService.getQuestions(id);
+    }
+
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable int id, @RequestBody List<Response> responses) {
+        return quizService.calculateResult(id, responses);
     }
 }
